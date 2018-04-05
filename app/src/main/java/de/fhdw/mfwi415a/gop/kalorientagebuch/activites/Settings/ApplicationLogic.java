59707495d8cb45158a2d993aa4e891b72db617f2
simonebaum <Baum_simone97@gmx.de>
2018-04-05 18:01:59 +0200

    Settings/Profil Activity

--- app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Settings/ApplicationLogic.java
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Settings/ApplicationLogic.java
@@ -6,10 +6,6 @@ import android.widget.Button;
 
 public class ApplicationLogic {
 
-    private Button button_name;
-    private Button button_hoechstsatz;
-    private Button neue_Einheit;
-    private Button alle_Einheiten;
 
     private Gui mGui;
     private Context mContext;
--- app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/navigation/fragments/ProfilFragment.java
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/navigation/fragments/ProfilFragment.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import de.fhdw.mfwi415a.gop.kalorientagebuch.R;
 import de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Settings.ApplicationLogic;
+import de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Settings.Dialog;
 import de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Settings.Gui;
 
 /**
@@ -17,6 +19,11 @@ import de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Settings.Gui;
 public class ProfilFragment extends android.app.Fragment {
 
     View myView;
+
+    private Button button_name;
+    private Button button_hoechstsatz;
+    private Button neue_Einheit;
+    private Button alle_Einheiten;
 
     private Gui mGui;
     private ApplicationLogic mApplicationLogic;
@@ -30,5 +37,24 @@ public class ProfilFragment extends android.app.Fragment {
         mGui = new Gui(myView);
         mApplicationLogic = new ApplicationLogic(mGui,getActivity() );
         return myView;
+
+        button_name = (Button) findViewById(R.id.button_name);
+        button_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+        button_hoechstsatz = (Button) findViewById(R.id.button_hoechstsatz);
+      //  button_neueEinheit = (Button) findViewById(R.id.button_neueEinheit);
+      //  button_alleEinheiten = (Button) findViewById(R.id.button_alleEinheiten);
+
+    }
+
+    public void openDialog(){
+
+        Dialog dialog = new Dialog();
+        dialog.show(getSupportFragmentManager(), "Dialog");
+
     }
 }
