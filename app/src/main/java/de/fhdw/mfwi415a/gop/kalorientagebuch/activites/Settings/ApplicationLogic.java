c15c281be5c5447b63e4fa31e1c83bac572cdde0
simonebaum <Baum_simone97@gmx.de>
2018-04-05 17:22:30 +0200

    Settings/Profil Activity

--- app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Settings/ApplicationLogic.java
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Settings/ApplicationLogic.java
@@ -1,8 +1,15 @@
 package de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Settings;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class ApplicationLogic {
+
+    private Button button_name;
+    private Button button_hoechstsatz;
+    private Button neue_Einheit;
+    private Button alle_Einheiten;
 
     private Gui mGui;
     private Context mContext;
