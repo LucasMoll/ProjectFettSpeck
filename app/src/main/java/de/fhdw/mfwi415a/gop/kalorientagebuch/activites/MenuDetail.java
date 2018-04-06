3645e4d2080fc2ad531207c7da62ffa9dd2eaf65
Lucas Moll <LucasMoll@users.noreply.github.com>
2018-04-06 08:49:51 +0200

    added activity menu_detail

---
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/MenuDetail.java
@@ -0,0 +1,15 @@
+package de.fhdw.mfwi415a.gop.kalorientagebuch.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.fhdw.mfwi415a.gop.kalorientagebuch.R;
+
+public class MenuDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_detail);
+    }
+}
