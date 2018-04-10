f8573d4097819872d47149a405ae8b7735f2bf0e
simonebaum <Baum_simone97@gmx.de>
2018-04-10 11:12:01 +0200

    Settings/Profil Activity

--- app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Menues/ApplicationLogic.java
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Menues/ApplicationLogic.java
@@ -1,4 +1,30 @@
 package de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Menues;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+
 public class ApplicationLogic {
+
+
+    private Gui mGui;
+    private Context mContext;
+
+    public ApplicationLogic (Gui gui, Context context) {
+        mGui = gui;
+        mContext = context;
+        initGui();
+        initListener();
+    }
+
+    private void initGui() {
+
+    }
+
+    private void initListener() {
+        ClickListener cl;
+
+        cl = new ClickListener(this);
+    }
+
 }
--- app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Menues/Gui.java
+++ app/src/main/java/de/fhdw/mfwi415a/gop/kalorientagebuch/activites/Menues/Gui.java
@@ -1,4 +1,12 @@
 package de.fhdw.mfwi415a.gop.kalorientagebuch.activites.Menues;
 
+import android.view.View;
+
 public class Gui {
+
+
+    public Gui(View myView) {
+
+    }
+
 }
